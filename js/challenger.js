5b1aaad8294cafa41c3b13998907a19c7812f768
Geraldo Costa <geraldocoosta@gmail.com>
2018-11-26 13:08:32 -0200

    mais mudanças

--- js/challenger.js
+++ js/challenger.js
@@ -1,36 +1,6 @@
 (function (DOM) {
   'use strict';
 
-  /*
-  Vamos estruturar um pequeno app utilizando módulos.
-  Nosso APP vai ser um cadastro de carros. Vamos fazê-lo por partes.
-  A primeira etapa vai ser o cadastro de veículos, de deverá funcionar da
-  seguinte forma:
-  - No início do arquivo, deverá ter as informações da sua empresa - nome e
-  telefone (já vamos ver como isso vai ser feito)
-  - Ao abrir a tela, ainda não teremos carros cadastrados. Então deverá ter
-  um formulário para cadastro do carro, com os seguintes campos:
-    - Imagem do carro (deverá aceitar uma URL)
-    - Marca / Modelo
-    - Ano
-    - Placa
-    - Cor
-    - e um botão "Cadastrar"
-  Logo abaixo do formulário, deverá ter uma tabela que irá mostrar todos os
-  carros cadastrados. Ao clicar no botão de cadastrar, o novo carro deverá
-  aparecer no final da tabela.
-  Agora você precisa dar um nome para o seu app. Imagine que ele seja uma
-  empresa que vende carros. Esse nosso app será só um catálogo, por enquanto.
-  Dê um nome para a empresa e um telefone fictício, preechendo essas informações
-  no arquivo company.json que já está criado.
-  Essas informações devem ser adicionadas no HTML via Ajax.
-  Parte técnica:
-  Separe o nosso módulo de DOM criado nas últimas aulas em
-  um arquivo DOM.js.
-  E aqui nesse arquivo, faça a lógica para cadastrar os carros, em um módulo
-  que será nomeado de "app".
-  */
-
   function app() {
 
     let $campoImagemCarro = new DOM('[data-js="imgCarro"]');
@@ -116,12 +86,12 @@
 
     function initializing() {
 
-      getURL('company.json', function (content, error) {
-        if (error != null) {
-          console.log('Erro na requisição ' + error);
-        }
-        setNomeETelefone(content);
-      });
+      // getURL('company.json', function (content, error) {
+      //   if (error != null) {
+      //     console.log('Erro na requisição ' + error);
+      //   }
+      //   setNomeETelefone(content);
+      // });
 
       $butaoEnviar.on('click', function (e) {
         e.preventDefault();
@@ -133,15 +103,11 @@
         setCarroInTable();
         limparCampos();
       });
-
     }
 
     return {
       init: initializing()
     }
-
-
   }
-
-  app();
+  app().init;
 })(window.DOMLib);
